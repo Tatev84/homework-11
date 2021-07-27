1b2e0850aa2dc61f2d1dd5b68e0d32dc4df69192
Tatev84 <tatevh84@gmail.com>
2021-07-27 23:37:35 +0400

    homework 11

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+
+/* let str="Im anunn e Anna";
+str=str.split("n")
+ console.log(str.join("xy"))
+ */
+
+/* function sym(str) {
+    for (i = 0; i < str.length / 2; i++) {
+        if (str[i] === str[str.length - 1 - i]) {
+            console.log(true)
+        } else {
+
+            console.log(false)
+
+        }
+    }
+}
+sym(prompt()) */
+
+/* function FullName(){
+
+    let name = prompt("What is you name?")
+    let surname = prompt("What is you surname?")
+    let MiddleName = prompt("What is you middle name?")
+    return (name +  surname  +  MiddleName)
+}
+
+alert(FullName())
+ */
+
+
+
+var students=["Տիգրան", "John", "Սամվել"]
+for (var i=0; i<students.length; i++){
+    if(students.charCodeAt[i]<1329 || students.charCodeAt[i] >1414){
+        console.log(students.charAt[i])
+    }
+
+    
+}
+
+
+        
+        
